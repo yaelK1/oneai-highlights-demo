927173f958200aaf017e2013f5fe2e618f908c6a
yaelk1 <yaelk@oneai.com>
2023-07-17 16:28:43 +0300

    asfsdf

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 import streamlit as st
-
+from st_aggrid import AgGrid
+import pandas as pd
 from processing import process_pdf
+from PIL import Image
+
+
+def show_results(highlights):
+    df = pd.DataFrame(
+        [[h.highlight, h.score, h.pages, h.segment_text] for h in highlights],
+        columns=["highlight", "score", "pages", "segment_text"],
+    )
+    AgGrid(df)
+
+
+def display_pdf(pdf_file):
+    pdf_display = f'<iframe src="data:application/pdf;base64,{pdf_file}" width="700" height="950" type="application/pdf"></iframe>'
+    st.markdown(pdf_display, unsafe_allow_html=True)
 
 
 def start_app():
-    st.title("One AI demo")
+    image = Image.open("logo.png")
+    col1, col2 = st.columns([2, 1])
+    col1.title("One AI demo")
+    col2.image(image, width=200)
     st.write("Upload PDF file to extract highlights from it")
     api_key = st.text_input("Enter your API key")
     highlight_amount = st.selectbox(
@@ -16,7 +34,9 @@ def start_app():
         if api_key is None:
             st.write("Please enter your API key")
         elif pdf_file is not None:
-            pdf_bytes = pdf_file.getvalue()
-            process_pdf(highlight_amount, pdf_file, api_key)
+            highlights = process_pdf(highlight_amount, pdf_file, api_key)
+            show_results(highlights)
+            pdf_file = pdf_file.getvalue().decode("utf-8")
+            display_pdf(pdf_file)
         else:
             st.write("Please upload a PDF file")
--- processing.py
+++ processing.py
@@ -1,19 +1,44 @@
 import oneai
 import asyncio
 import streamlit as st
+import pandas as pd
+import streamlit.components.v1 as components
 
 
 def process_pdf(highlight_amount, pdf_bytes, api_key):
+    print(pdf_bytes)
     oneai.api_key = api_key
-    input = oneai.Input(text=pdf_bytes, content_type="pdf")
     higighlights_pipeline = oneai.Pipeline(
         [oneai.skills.Highlights(params={"amount": highlight_amount})]
     )
     segments_pipeline = oneai.Pipeline(
         [
             oneai.skills.PDFExtractText(params={"engine": "v2"}),
-            oneai.skills.SplitByTopic(),
+            oneai.skills.SplitByTopic(amount=highlight_amount),
         ]
     )
-    segments = asyncio.run(segments_pipeline.run_async(input))
-    st.write(segments)
+    output = asyncio.run(segments_pipeline.run_async(pdf_bytes))
+    segments = output.pdf_text.segments
+    pages = [l for l in output.pdf_text.pdf_labels if l.name == "PAGE"]
+    highlights = []
+    for segment in segments:
+        segment_output = higighlights_pipeline.run(segment.span_text)
+        segment_highlights = segment_output.highlights
+        offset = segment.output_spans[0].start
+        for highlight in segment_highlights:
+            highlight.pages = []
+            highlight.segment_text = segment.span_text
+            highlight.start = offset + int(highlight.output_spans[0].start)
+            highlight.end = offset + int(highlight.output_spans[0].end)
+            highlight.score = float(highlight.data["score"])
+            highlight.highlight = highlight.span_text
+            for page in pages:
+                page.start = int(page.output_spans[0].start)
+                page.end = int(page.output_spans[0].end)
+                if (
+                    page.start <= highlight.start <= page.end
+                    or page.start <= highlight.end <= page.end
+                ):
+                    highlight.pages.append(page.data["numeric_value"])
+            highlights.append(highlight)
+    return highlights
