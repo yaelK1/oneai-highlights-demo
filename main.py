c42c2d9ec529e01c134cf38e71b3516285da888e
yaelk1 <yaelk@oneai.com>
2023-07-18 11:40:04 +0300

    print errors

--- main.py
+++ main.py
@@ -47,8 +47,11 @@ def start_app():
             with open("uploaded_file.pdf", "wb") as f:
                 f.write(pdf_file.getvalue())
             with open("uploaded_file.pdf", "rb") as f:
-                highlights = process_pdf(highlight_amount, f, api_key)
-                show_results(highlights)
+                try:
+                    highlights = process_pdf(highlight_amount, f, api_key)
+                    show_results(highlights)
+                except Exception as e:
+                    st.write(e)
             with open("uploaded_file.pdf", "rb") as f:
                 base64_pdf = base64.b64encode(f.read()).decode("utf-8")
                 pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
